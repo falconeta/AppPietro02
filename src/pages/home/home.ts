480f6a2333b41ad3fd89f0623cb0e43a60702cb6
Vittorio Lo Preiato <vittorio.falconeta@gmail.com>
2018-03-20 10:43:19 +0100

    aggiunto due elementi all'array oggetti di test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AddModifyPage } from '../add-modify/add-modify';
+import { Oggetto } from '../../models/oggetto';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  oggetti: Oggetto[] = [];
   constructor(public navCtrl: NavController) {
 
   }
